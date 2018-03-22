5bcd1ce2054ef3f33e39451885b28e4d4b0dd559
Aleksei Smelianski <alekseismelianski@alekseis-mbp.fios-router.home>
2018-03-22 17:26:51 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Aleksei Smelianski on 3/22/18.
+//  Copyright © 2018 Aleksei Smelianski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
